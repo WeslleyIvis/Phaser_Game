5bb2b045b650c8a28f60f8f6439cfe616d98be08
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-06 20:15:14 -0300

    Controller Scene

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 // Autocompleta os types do phaser
 /** @type {import("../typings/phaser")} */
 
+import { LoadScene } from './scenes/LoadScene';
+import { MenuScene } from './scenes/MenuScene';
+
 let game = new Phaser.Game({
   width: 100,
   height: 100,
+  scene: [LoadScene, MenuScene],
 });
