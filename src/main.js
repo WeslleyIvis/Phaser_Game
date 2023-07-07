58ba5ee4eec3575b8bc04bc8e2582534a01c2510
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-07 05:09:33 -0300

    game window responsive

--- src/main.js
+++ src/main.js
@@ -7,7 +7,14 @@ import { LoadScene } from './scenes/LoadScene';
 import { MenuScene } from './scenes/MenuScene';
 
 let game = new Phaser.Game({
-  width: 100,
-  height: 100,
+  width: 800,
+  height: 600,
   scene: [LoadScene, MenuScene],
+  render: {
+    pixelArt: true,
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 });
