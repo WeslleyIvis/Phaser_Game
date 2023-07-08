c6278c864b4289e9963f7f9ac521fe1b1ac98a79
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-08 00:34:10 -0300

    JS-SRC

---
+++ js-src/scenes/LoadScene.js
@@ -0,0 +1,66 @@
+import { CST } from '../CST';
+export class LoadScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: CST.SCENES.LOAD,
+        });
+    }
+    init() { }
+    loadImages() {
+        this.load.setPath("./assets/image");
+        for (let prop in CST.IMAGE) {
+            // @ts-ignore
+            this.load.image(CST.IMAGE[prop], CST.IMAGE[prop]);
+        }
+    }
+    loadAudio() {
+        this.load.setPath("./assets/audio");
+        for (let prop in CST.AUDIO) {
+            //@ts-ignore
+            this.load.audio(CST.AUDIO[prop], CST.AUDIO[prop]);
+        }
+    }
+    loadSprit(frameConfig) {
+        this.load.setPath("./assets/sprites");
+        for (let prop in CST.SPRITE) {
+            //@ts-ignore
+            this.load.spritesheet(CST.SPRITE[prop], CST.SPRITE[prop], frameConfig);
+        }
+    }
+    preload() {
+        // Load image, spritesheet, sound
+        this.loadImages();
+        this.loadAudio();
+        this.loadSprit({
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        const loadingTitle = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Loading', {
+            fontFamily: 'Arial',
+            fontSize: '40px',
+            color: '#ffffff',
+        });
+        loadingTitle.setOrigin(0.5, 2);
+        // Create loading bar
+        /*
+        Loader Events:
+          Complete - when done loading everything.
+          Progress - loader number progress in decimal.
+        */
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff,
+            },
+        });
+        this.load.on('progress', (percent) => {
+            loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
+        });
+        this.load.on('load', (file) => {
+            console.log(file.src);
+        });
+    }
+    create() {
+        this.scene.start(CST.SCENES.MENU);
+        // this.scene.launch();
+    }
+}
