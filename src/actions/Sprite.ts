17c7e4b9470910f1e54ee3d505ba6cad0df3ef4b
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-11 23:47:41 -0300

    Collision and movemnt

---
+++ src/actions/Sprite.ts
@@ -0,0 +1,10 @@
+export default class Sprite extends Phaser.GameObjects.Sprite {
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
+        super(scene, x, y, texture, frame);
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this)
+        this.setScale(1);
+        this.setOrigin(0, 0);
+    }
+}
