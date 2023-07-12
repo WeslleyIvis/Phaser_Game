34c94798b6c2700d2173aef84beedfba0c161bf1
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-11 23:46:22 -0300

    Create character class

---
+++ js-src/actions/CharacterSprite.js
@@ -0,0 +1,12 @@
+export default class CharacterSprite extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this);
+        this.setScale(1);
+        this.setOrigin(0, 0);
+        scene.physics.world.enableBody(this);
+        this.setImmovable(true);
+        this.hp = 10;
+    }
+}
