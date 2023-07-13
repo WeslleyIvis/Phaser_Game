c540a55de72f06bef1e19c34c39feac910d85c0f
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-13 04:57:55 -0300

    Character Anims Module

---
+++ src/anims/CharacterAnims.ts
@@ -0,0 +1,51 @@
+import Phaser from "phaser";
+
+const createCharacterAnims = (anims: Phaser.Animations.AnimationManager) => {
+    anims.create({
+        key: 'down',
+        frameRate: 6,
+        repeat: 0,
+        frames: anims.generateFrameNames('characters', {
+            prefix: 'samira-front',
+            start: 0,
+            end: 2
+        })
+    })
+
+    anims.create({
+        key: 'up',
+        frameRate: 6,
+        repeat: 0,
+        frames: anims.generateFrameNames('characters', {
+            prefix: 'samira-back',
+            start: 0,
+            end: 2
+        })
+    })
+
+    anims.create({
+        key: 'left',
+        frameRate: 6,
+        repeat: 0,
+        frames: anims.generateFrameNames('characters', {
+            prefix: 'samira-left',
+            start: 0,
+            end: 2
+        })
+    })
+
+    anims.create({
+        key: 'right',
+        frameRate: 6,
+        repeat: 0,
+        frames: anims.generateFrameNames('characters', {
+            prefix: 'samira-right',
+            start: 0,
+            end: 2
+        })
+    })    
+}
+
+export {
+    createCharacterAnims
+}
