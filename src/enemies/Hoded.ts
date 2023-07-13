c17ebdc678e220e4acb7e133390a8cb7e2551539
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-13 04:57:30 -0300

    Hoded CLass

---
+++ src/enemies/Hoded.ts
@@ -0,0 +1,5 @@
+export class Hoded extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
+        super(scene, x, y, texture, frame)
+    }
+}
