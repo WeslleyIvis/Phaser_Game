0377b2acfb8cf49d2298ed1e71060fdcebaa9ccb
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-16 05:44:20 -0300

    Default Class

--- src/scenes/LoadScene.ts
+++ src/scenes/LoadScene.ts
@@ -1,6 +1,6 @@
 import { CST } from '../CST';
 
-export class LoadScene extends Phaser.Scene {
+export default class LoadScene extends Phaser.Scene {
   constructor() {
     super({
       key: CST.SCENES.LOAD,
--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -5,19 +5,22 @@ import "../characters/Samira";
 import Bat from "../enemies/Bat";
 import Hoded from "../enemies/Hoded";
 import { createSpellBubleAnims } from "../anims/SpellsAnims";
-export class PlayScene extends Phaser.Scene {
+import Samira from "../characters/Samira";
+export default class PlayScene extends Phaser.Scene {
     private cursor!: Phaser.Types.Input.Keyboard.CursorKeys
-    character!: Phaser.Physics.Arcade.Sprite;
-    atackes!: Phaser.Physics.Arcade.Group;
-    //keyboard!: {[index: string] : Phaser.Input.Keyboard.Key};
+    private character!: Samira;
 
+    private bats!: Phaser.Physics.Arcade.Group;
+
+    atackes!: Phaser.Physics.Arcade.Group;
     constructor() {
         super({key: CST.SCENES.PLAY})
     }
 
     preload() {
         //console.log(this.textures.list)     
-        this.cursor = this.input.keyboard?.createCursorKeys() as Phaser.Types.Input.Keyboard.CursorKeys;
+
+        this.cursor = this.input.keyboard?.addKeys(CST.KEYBOARD.KEYS) as Phaser.Types.Input.Keyboard.CursorKeys
 
         this.load.image("tiles", "./assets/maps/textures.png");
         this.load.image("itens", "./assets/maps/itens.png");
@@ -25,13 +28,17 @@ export class PlayScene extends Phaser.Scene {
     }
 
     create() {
+        this.scene.run(CST.SCENES.GAME_UI)
+
         createSpellBubleAnims(this.anims)
         createCharacterAnims(this.anims)
         createHodedAnims(this.anims)
         createBatAnims(this.anims)
 
-        this.character = this.add.samira(500, 500, 'characters')
+        this.character = this.add.samira(500, 500, 'characters').setSize(30,50).setOffset(10, 20)
+
         this.atackes = this.physics.add.group();
+
         const hodeds = this.physics.add.group({
             classType: Hoded,
             createCallback: (go) => {
@@ -51,14 +58,12 @@ export class PlayScene extends Phaser.Scene {
 
         hodeds.get(400, 400, 'enemies', 'ghost-front1')
         bats.get(500, 500, 'enemies', 'bat-front1')
+        bats.get(500, 600, 'enemies', 'bat-front1')
+        bats.get(500, 700, 'enemies', 'bat-front1')
               
         //@ts-ignore
         window.character = this.character
-
-        // Create keyboards && events 
-        //@ts-ignore
-        this.keyboard = this.input.keyboard.addKeys(CST.KEYBOARD.KEYS)
-
+      
         this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {        
             if(pointer.isDown) { //is clicking
                 let magic = this.physics.add.sprite(pointer.worldX, pointer.worldY, "magicEffect", "magic1").play("spellBuble").setSize(50,50).setOffset(20, 35)
@@ -94,15 +99,29 @@ export class PlayScene extends Phaser.Scene {
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
         this.cameras.main.setDeadzone(this.scale.width * 0.1, this.scale.height * 0.1)
 
-
-
         objcollider?.setCollisionByProperty({collider: true})
-        this.physics.world.addCollider(this.character, bats)
+        
+        //this.physics.world.addCollider(this.character, bats)
         this.physics.add.collider(this.character, objcollider as Phaser.Tilemaps.TilemapLayer)
         this.physics.add.collider(bats, objcollider as Phaser.Tilemaps.TilemapLayer);
+
+        this.physics.add.collider(bats, this.character, this.handlePlayerBatCollision as Phaser.Types.Physics.Arcade.ArcadePhysicsCallback, undefined, this)
     }  
-    //@ts-ignore
-    update(time, delta) { //delta 16.666 @ 60fps
+
+    private handlePlayerBatCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+        const bat = obj2 as Bat
+
+        const dx = this.character.x - bat.x
+        const dy = this.character.y - bat.y
+
+        const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200)
+
+        this.character.handleDamege(dir)
+
+    }
+
+    
+    update(time: any, delta: any) { //delta 16.666 @ 60fps
 
         if(this.character) {
             this.character.update(this.cursor)
