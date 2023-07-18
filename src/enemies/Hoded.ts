921e7b133006a8af52e1e0893cffd5b28767fc68
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-18 02:44:18 -0300

    Sprite

--- src/enemies/Hoded.ts
+++ src/enemies/Hoded.ts
@@ -8,7 +8,6 @@ enum Direction {
 export default class Hoded extends Phaser.Physics.Arcade.Sprite {
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
         super(scene, x, y, texture, frame)
-        this.anims.play('assassin-down')
     }
 
     protected preUpdate(time: number, delta: number): void {
