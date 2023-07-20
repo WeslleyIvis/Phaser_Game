396532b5ed2c41f11a07d271a251ff3d165668c0
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-20 03:21:41 -0300

    Bats droped Hearts

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -10,11 +10,14 @@ import Character from "../characters/Character";
 
 import Bat from "../enemies/Bat";
 import Hoded from "../enemies/Hoded";
+import Item from "../items/item";
 export default class PlayScene extends Phaser.Scene {
     private cursor!: Phaser.Types.Input.Keyboard.CursorKeys
     private character!: Character;
     
     private atackes!: Phaser.Physics.Arcade.Group
+
+    private items!: Phaser.Physics.Arcade.Group
 
     private bats!: Phaser.Physics.Arcade.Group
 
@@ -31,7 +34,7 @@ export default class PlayScene extends Phaser.Scene {
 
         this.load.image("tiles", "./assets/maps/textures.png");
         this.load.image("itens", "./assets/maps/itens.png");
-        this.load.tilemapTiledJSON("map", "./assets/maps/mappy.json");
+        this.load.tilemapTiledJSON("map", "./assets/maps/mappy1.json");
     }
 
     create() {
@@ -50,10 +53,17 @@ export default class PlayScene extends Phaser.Scene {
             }
         });
 
+        this.items = this.physics.add.group({
+            classType: Item
+        })
+
+        this.items.get(500, 500, CST.IMAGE.HEART_EMPTY)
+
 
         this.character = this.add.character(700, 100, 'characters')
-        this.character.setScale(0.9)
+        this.character.setScale(0.85)
         this.character.setAtackes(this.atackes)
+        this.character.setDepth(1);
         
         window.char = this.character
         
@@ -77,7 +87,7 @@ export default class PlayScene extends Phaser.Scene {
         hodeds.get(400, 400, 'enemies', 'demon-gargoyle-front1')
         
 
-        for(let x = 0; x < 15; x++)
+        for(let x = 0; x < 1; x++)
         {
             this.bats.get(Phaser.Math.Between(400, 800), Phaser.Math.Between(400, 900), 'enemies', 'bat-front1')
         }
@@ -88,19 +98,41 @@ export default class PlayScene extends Phaser.Scene {
         const ground = map.createLayer("floor", tileset, 0, 0)?.setDepth(-2)
         const groundAbove = map.createLayer('floor_above', tileset, 0, 0)?.setDepth(-1)
         const shadow = map.createLayer("shadow", tileset, 0, 0)
+        const shadow_2 = map.createLayer("shadow_2", tileset, 0, 0)
         const objcollider = map.createLayer("collider", tileset, 0, 0)
-        const objabove = map.createLayer("above", tileset, 0, 0)?.setDepth(1)
+        const objcollider_2 = map.createLayer("collider_2", tileset, 0, 0)
+        const objabove = map.createLayer("above", tileset, 0, 0)?.setDepth(2)
+        const objabove_2 = map.createLayer("above_2", tileset, 0, 0)?.setDepth(2)
 
-        this.input.on("gameobjectdown", (pointer: Phaser.Input.Pointer, obj: Phaser.GameObjects.GameObject) => {
-            obj.destroy()
+        const tileColliderGroup = map.getObjectLayer('tiles_collider')
+        const staticTileGroup = this.physics.add.staticGroup()
+
+        tileColliderGroup?.objects.forEach((tile) => {
+            const objectX = tile.x as number + (tile.width as number) / 2; // Adiciona a metade da largura para centralizar o objeto
+            const objectY = tile.y as number + (tile.height as number)  / 2; // Adiciona a metade da altura para centralizar o objeto
+        
+            const tileCollider = staticTileGroup.create(objectX, objectY, undefined) as Phaser.Physics.Arcade.Image
+
+            tileCollider.setSize(tile.width as number, tile.height as number)
+            tileCollider.setVisible(false)
+
+            tileCollider.setImmovable(true);
         })
+
+        this.physics.add.collider(this.character, staticTileGroup)
+        this.physics.add.collider(this.bats, staticTileGroup)
 
         this.cameras.main.startFollow(this.character);
         this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
-        this.cameras.main.setDeadzone(this.scale.width * 0.1, this.scale.height * 0.1)
+        // this.cameras.main.setDeadzone(this.scale.width * 0.1, this.scale.height * 0.1)
+        //this.cameras.main.setZoom(1.2)
+
+        window.can = this.cameras
 
         objcollider?.setCollisionByProperty({collider: true})
+        objcollider_2?.setCollisionByProperty({collider: true})
+        
         
         this.physics.add.collider(this.character, objcollider as Phaser.Tilemaps.TilemapLayer)
         
@@ -110,6 +142,10 @@ export default class PlayScene extends Phaser.Scene {
 
         this.physics.add.collider(this.bats, objcollider as Phaser.Tilemaps.TilemapLayer);
         this.playerCollider = this.physics.add.collider(this.bats, this.character, this.handlePlayerBatCollision as Phaser.Types.Physics.Arcade.ArcadePhysicsCallback, undefined, this)
+
+        this.physics.add.collider(this.items, objcollider as Phaser.Tilemaps.TilemapLayer)
+        this.physics.add.collider(this.items, this.character, this.handleItemCollision as Phaser.Types.Physics.Arcade.ArcadePhysicsCallback, undefined, this)
+        
     }  
 
     private handleAtackWallCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
@@ -117,9 +153,14 @@ export default class PlayScene extends Phaser.Scene {
         obj1.destroy()
     }
     
-    private handleAtackeCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
-        this.bats.killAndHide(obj1)
-        this.bats.killAndHide(obj2)
+    private handleAtackeCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.Physics.Arcade.Sprite) {
+        console.dir({obj1, obj2})
+
+        // this.bats.killAndHide(obj1)
+        // this.bats.killAndHide(obj2)
+
+        this.items.get(obj2.x, obj2.y, CST.IMAGE.HEART_FULL)
+
         obj2.destroy()
         obj1.destroy()
     }
@@ -140,6 +181,17 @@ export default class PlayScene extends Phaser.Scene {
         {
             this.playerCollider?.destroy();
         }
+    }
+
+    handleItemCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+        
+        // Pega
+        if(this.character.health < this.character.maxHealth) {
+            this.character.recoverHealth();
+            obj2.destroy();
+        }
+
+        sceneEvents.emit('update-max-health-changed', this.character.health, this.character.maxHealth)
     }
 
     
