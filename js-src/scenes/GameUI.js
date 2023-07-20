9a6fd1d17781930bad4915a20ae18c5019d40aee
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-20 03:20:03 -0300

    TS-Comp

--- js-src/scenes/GameUI.js
+++ js-src/scenes/GameUI.js
@@ -3,10 +3,11 @@ import { sceneEvents } from "../events/EventCenter";
 export default class GameUI extends Phaser.Scene {
     constructor() {
         super(CST.SCENES.GAME_UI);
+        this.amountHearts = 3;
     }
     create() {
-        // this.add.rectangle(95, 47, 82, 10, 0xff0000)
-        // const bar = this.add.image(48, -64, CST.IMAGE.LIFE_BAR).setOrigin(0).setCrop(0, 48, 48, 16).setScale(2)
+        this.add.rectangle(95, 47, 82, 10, 0xff0000);
+        const bar = this.add.image(48, -64, CST.IMAGE.LIFE_BAR).setOrigin(0).setCrop(0, 48, 48, 16).setScale(2);
         this.hearts = this.add.group({
             classType: Phaser.GameObjects.Image
         });
@@ -14,7 +15,7 @@ export default class GameUI extends Phaser.Scene {
             key: CST.IMAGE.HEART_FULL,
             setXY: {
                 x: 50,
-                y: 50,
+                y: 300,
             },
             quantity: 3,
         });
@@ -24,6 +25,7 @@ export default class GameUI extends Phaser.Scene {
             heart.setScale(2);
         });
         sceneEvents.on('player-health-changed', this.handlePlayerHealthChanged, this);
+        sceneEvents.on('update-max-health-changed', this.updateHeartCount, this);
         this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
             sceneEvents.off('player-health-changed', this.handlePlayerHealthChanged, this);
         });
@@ -38,6 +40,34 @@ export default class GameUI extends Phaser.Scene {
             else {
                 heart.setTexture(CST.IMAGE.HEART_EMPTY);
             }
+        });
+    }
+    updateHeartCount(currentHealth, maxHealth) {
+        maxHealth -= currentHealth;
+        console.log({ currentHealth, maxHealth });
+        this.hearts.clear(true);
+        this.hearts.createMultiple({
+            key: CST.IMAGE.HEART_FULL,
+            setXY: {
+                x: 50,
+                y: 50,
+            },
+            quantity: currentHealth
+        });
+        if (maxHealth) {
+            this.hearts.createMultiple({
+                key: CST.IMAGE.HEART_EMPTY,
+                setXY: {
+                    x: 50,
+                    y: 50,
+                },
+                quantity: maxHealth
+            });
+        }
+        //@ts-ignore
+        this.hearts.children.iterate((heart, index) => {
+            heart.x = 50 + (index * 30);
+            heart.setScale(2);
         });
     }
 }
