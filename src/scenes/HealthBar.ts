ee9c50385ee3218461d339de2bcd8f7d726e0ff0
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-27 23:56:51 -0300

    Unset

--- src/scenes/HealthBar.ts
+++ src/scenes/HealthBar.ts
@@ -16,7 +16,6 @@ export default class HealthBar extends Phaser.GameObjects.Co {
         this.maxValue = 100
 
         this.healthBar = new Phaser.GameObjects.Graphics(scene);
-        this.add
 
         this.draw()
         this.scene.add.existing(this);
