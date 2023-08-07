7711b294b9058ad65dc6bce6a7b8329f693cc79e
WeslleyIvis <weslley_reive@hotmail.com>
2023-08-07 04:45:36 -0300

    Sword add Scene

--- src/items/Sword.ts
+++ src/items/Sword.ts
@@ -1,9 +1,11 @@
-export default class Sword extends Phaser.GameObjects.Sprite {
+export default class Sword extends Phaser.Physics.Arcade.Sprite {
     private damage?: number
     private atackSpeed?: number
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number, damage?: number, atackSpeed?: number) {
         super(scene, x, y, texture, frame)
+
+        scene.add.existing(this)
         scene.physics.world.enable(this)
 
         this.damage = damage
