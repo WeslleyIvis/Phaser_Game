2c4ed4b2b3310953b94fadd693c71d78d81ad9fc
WeslleyIvis <79803635+WeslleyIvis@users.noreply.github.com>
2023-09-10 01:32:23 -0300

    Delete js-src directory

--- js-src/actions/Sprite.js
+++
@@ -1,9 +0,0 @@
-export default class Sprite extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
-        super(scene, x, y, texture, frame);
-        scene.sys.updateList.add(this);
-        scene.sys.displayList.add(this);
-        this.setScale(1);
-        this.setOrigin(0, 0);
-    }
-}
--- js-src/scenes/ConfingScene.js
+++
@@ -1,8 +0,0 @@
-export default {
-    followCamera: (camera, character, tileMap) => {
-        camera.main.startFollow(character);
-        camera.main.setBounds(0, 0, tileMap.widthInPixels, tileMap.heightInPixels);
-        camera.main.setZoom(1.2);
-        // camera.main.setDeadzone(this.scale.width * 0.1, this.scale.height * 0.1)
-    }
-};
